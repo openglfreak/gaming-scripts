49ceb499d91bd6f0d4d25d84d27679aad972943b
Torge Matthies <openglfreak@googlemail.com>
2019-01-02 23:05:25 +0100

    Better dbus signal waiting function.

--- lib/dbus.sh
+++ lib/dbus.sh
@@ -3,16 +3,22 @@
 # Waits for a dbus signal
 # params: path interface member
 dbus_wait_for_signal() {
-	dbus-monitor --session --profile "type=signal,path=$1,interface=$2,member=$3" |\
+	sh -c 'printf "%s\\n" "$$"; LINES= COLUMNS= LC_ALL=C exec dbus-monitor --session --profile "type=signal,path=$1,interface=$2,member=$3"' "$0" "$1" "$2" "$3" 2>/dev/null |\
 		{
+			# Read pid
+			IFS= read -r __dbus_wait_for_signal_mpid
+
 			# Skip table header
 			IFS= read -r
 			IFS= read -r
 
-			# Skip NameAcquired signal
-			IFS= read -r
+			# Wait for one matching line
+			while IFS= read -r line; do
+				case "$line" in *[[:space:]]"$1"[[:space:]]"$2"[[:space:]]"$3")
+					break
+				esac
+			done
 
-			# Wait for one line
-			IFS= read -r
+			kill "$__dbus_wait_for_signal_mpid"
 		}
 }
