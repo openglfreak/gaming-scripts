308888ffa1785b7dc3ea1311771a96ada50acc31
Torge Matthies <openglfreak@googlemail.com>
2019-01-04 18:34:28 +0100

    Remove deepin wm functions, as they have been implemented into Lutris.

--- osu.sh
+++ osu.sh
@@ -2,20 +2,15 @@
 
 . ./lib/pulseaudio.sh
 . ./lib/nvidia_oc.sh
-. ./lib/deepin_wm.sh
 
 # Stop pulseaudio
 stop_pulseaudio
 # Overclock graphics card
 enter_nvidia_oc
-# Disable window manager in DDE (causes massive input lag)
-disable_wm_deepin
 
 # Run osu!
 lutris lutris:rungame/osu
 
-# Restore window manager in DDE
-restore_wm_deepin
 # Undo graphics card overclock
 exit_nvidia_oc
 # Restore pulseaudio
--- quake-champions.sh
+++ quake-champions.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 . ./lib/nvidia_oc.sh
-. ./lib/deepin_wm.sh
 
 # Overclock graphics card
 enter_nvidia_oc
-# Disable window manager in DDE (causes massive input lag)
-disable_wm_deepin
 
 # Run Quake Champions
 lutris lutris:rungame/quake-champions
 
-# Restore window manager in DDE
-restore_wm_deepin
 # Undo graphics card overclock
 exit_nvidia_oc
